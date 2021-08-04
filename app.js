8e44181d048cd03b602c468e0715800dc60799a5
Matt Carter <m@ttcarter.com>
2021-08-04 12:46:52 +1000

    feat: Added support for settings files

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 #!/usr/bin/node
+import _ from 'lodash';
+import colors from 'chalk';
 import {promises as fs} from 'node:fs';
 import getColor from './lib/getColor.js';
+import joi from 'joi';
+import os from 'node:os';
+import path from 'path';
 import program from 'commander';
 import repl from 'node:repl';
 import ttys from 'ttys';
@@ -29,17 +34,56 @@ const settings = {
 };
 // }}}
 
+// Main promise
+Promise.resolve()
+	// Load settings from INI file if it exists {{{
+	.then(()=> Promise.all([
+		fs.readFile(path.join(os.homedir(), '.mongoosh.json'))
+			.then(contents => JSON.parse(contents))
+			.then(parsed => _.merge(settings, parsed))
+			.then(()=> true)
+			.catch(()=> false), // Give up if trying to read a non-existant file
 
-const replInstance = repl
-	.start({
-		// BUGFIX: If we are reading from a pipe we need ttys to provide us a user terminal rather than trust process.std{in,out} {{{
-		input: ttys.stdin,
-		output: ttys.stdout,
-		terminal: true,
-		// }}}
-		useColors: program.color,
-		prompt: getColor(settings.prompt.color)(settings.prompt.text),
-		writer: function(doc) {
-			return util.inspect(doc, settings.inspect);
-		},
-	});
+		fs.access(path.join(os.homedir(), '.mongoosh.js'))
+			.then(()=> import(path.join(os.homedir(), '.mongoosh.js')))
+			.then(parsed => _.merge(settings, parsed.default))
+			.then(()=> true),
+
+		fs.access(path.join(os.homedir(), '.mongoosh.mjs'))
+			.then(()=> import(path.join(os.homedir(), '.mongoosh.mjs')))
+			.then(parsed => _.merge(settings, parsed.default))
+			.then(()=> true)
+			.catch(()=> false),
+	]))
+	.then(configsLoaded =>
+		configsLoaded.some(Boolean) // We loaded some config?
+		&& joi.object({ // Check it validates
+			inspect: {
+				depth: joi.number(),
+				colors: joi.boolean(),
+			},
+			prompt: {
+				text: joi.string(),
+				color: joi.string(),
+			},
+		}).validate(settings)
+	)
+
+	// }}}
+	// Repl loop {{{
+	.then(()=> new Promise(resolve => repl
+		.start({
+			// BUGFIX: If we are reading from a pipe we need ttys to provide us a user terminal rather than trust process.std{in,out} {{{
+			input: ttys.stdin,
+			output: ttys.stdout,
+			terminal: true,
+			// }}}
+			useColors: program.color,
+			prompt: getColor(settings.prompt.color)(settings.prompt.text),
+			writer: function(doc) {
+				return util.inspect(doc, settings.inspect);
+			},
+		})
+		.on('exit', resolve)
+	))
+	.catch(e => console.log(colors.red('ERROR', e)))
